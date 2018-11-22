9460a81b3f5f035754a7169d41f6ed8d93231f8f
Sarah Paz <sarah.paz@gmail.com>
2018-11-22 15:02:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@ board = []
 index = 0
 
 def position_taken?(board, index)
-  if board[index] == " " || ""
+  if board[index] == " " || board[index] == ""
     return false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   else
     return false
